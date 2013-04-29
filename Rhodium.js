cb05d4e17e6dfb3f5cfb0c0fb36cc3b68bbe6a4c
Eric DeMone <eric.demone@gmail.com>
2013-04-29 09:04:18 -0400

    All code as of April 29, 2013
    YAY FIRST REPOSITORY!

---
+++ Rhodium.js
@@ -0,0 +1,451 @@
+var numHomophoneTypes = 2;
+var time = 0;
+var gasPercent = 0;
+var difficulty = 3;
+var gameRunning = false;
+var gameTick;
+var highScore = 0;
+var multiplier = 0.01;
+var gasChanger = 0.05;
+var gasAdded = 0;
+var addGasOn = false;
+var questionType = 0;
+var questionNumber = 0;
+var ansNum = 0;
+var money = 0;
+var selectedDiv = 0;
+var status = 1;
+var influence = 0;
+var score = 0;
+
+var questionArray = new Array();
+var answerArray = new Array();
+
+function selected(n)
+{
+    selectedDiv=n;
+//    answer1.style.backgroundColor = "#ffffff";
+//    answer2.style.backgroundColor = "#ffffff";
+//    answer3.style.backgroundColor = "#ffffff";
+//    switch(n)
+//    {
+//        case 1:
+//        answer1.style.backgroundColor="#cccccc";
+//        break;
+//        case 2:
+//        answer2.style.backgroundColor="#cccccc";
+//        break;
+//        case 3:
+//        answer3.style.backgroundColor="#cccccc";
+//        break;
+//    }
+//    if(selectedDiv == n)
+//    {
+        answered();
+//    }
+//    else
+//    {
+//        selectedDiv=n;
+//    }
+}
+
+function answered()
+{
+    var isCorrect = false;
+    if(selectedDiv != 0)
+    {
+        switch(selectedDiv)
+        {
+            case 1:
+                if(document.getElementById("answer1").innerHTML == answerArray[questionType][(questionArray[questionType][questionNumber][false]) + 1])
+                {
+    //                alert("right!");
+                    influence+=Math.floor(Math.random()*10+1)*difficulty;
+                    isCorrect = true;
+                }
+                else
+                {
+    //                alert("wrong!");
+                    influence-=Math.floor(Math.random()*20+1)*difficulty*3;
+                    gasPercent-=5;
+                }
+            break;
+            case 2:
+                if(document.getElementById("answer2").innerHTML == answerArray[questionType][(questionArray[questionType][questionNumber][false]) + 1])
+                {
+    //                alert("right!");
+                    influence+=Math.floor(Math.random()*10+1)*difficulty;
+                    isCorrect = true;
+                }
+                else
+                {
+    //                alert("wrong!");
+                    influence-=Math.floor(Math.random()*20+1)*difficulty*3;
+                    gasPercent-=5;
+                }
+            break;
+            case 3:
+                if(document.getElementById("answer3").innerHTML == answerArray[questionType][(questionArray[questionType][questionNumber][false]) + 1])
+                {
+    //                alert("right!");
+                    influence+=Math.floor(Math.random()*10+1)*difficulty;
+                    isCorrect = true;
+                }
+                else
+                {
+    //                alert("wrong!");
+                    influence-=Math.floor(Math.random()*20+1)*difficulty*3;
+                    gasPercent-=5;
+                }
+        }
+        answerSelectionText.style.visibility = 'hidden';
+        answer1.style.visibility = 'hidden';
+        answer2.style.visibility = 'hidden';
+        answer3.style.visibility = 'hidden';
+        if(isCorrect==true)
+        {
+//                alert("Right");
+            if(statusP.innerHTML=="Transport passenger!")
+            {
+                sendObstruction(true);
+                setTimeout(function(){askQuestion()}, 3700);
+            }
+            else
+            {
+                setTimeout(function(){askQuestion()}, 1000);
+            }
+            if(status == 1)
+            {
+                statusP.innerHTML = "Transport passenger!";
+                if(Math.floor(Math.random()*4) == 0 && influence>9*difficulty)
+                {
+                   status=2; 
+                }
+                influence=9*difficulty;
+            }
+            else if(status == 2)
+            {
+                statusP.innerHTML = "Drop off passenger!";
+                status=3;
+            }
+            else
+            {
+                status=1;
+                money+=influence;
+                statusP.innerHTML = "Pick up passenger!";
+            }
+            score+=difficulty*difficulty*20;
+        }
+        else
+        {
+//                 alert("Wrong");
+            score-=difficulty*difficulty*100;
+            if(statusP.innerHTML=="Transport passenger!")
+            {
+                sendObstruction(false);
+                setTimeout(function(){askQuestion()}, 3700);
+            }
+            else
+            {
+                setTimeout(function(){askQuestion()}, 1000);
+            }
+        }
+        if(influence<0)
+        {
+            influence = 0;
+        }
+    }
+}
+
+function askQuestion()
+{
+    if(gameRunning)
+    {
+        answerSelectionText.style.visibility = 'visible';
+        answer1.style.visibility = 'visible';
+        answer2.style.visibility = 'visible';
+        answer3.style.visibility = 'visible';
+    }
+    selectedDiv=0;
+//    answer1.style.backgroundColor = "#ffffff";
+//    answer2.style.backgroundColor = "#ffffff";
+//    answer3.style.backgroundColor = "#ffffff";
+    
+    questionType = Math.floor(Math.random() * numHomophoneTypes);
+    questionNumber = Math.floor(Math.random() * 5);
+    
+    answerSelectionText.innerHTML = questionArray[questionType][questionNumber][true];
+    
+    ansNum=Math.floor(Math.random()*3);
+    for(var i = 0; i<3; i++)
+    {
+        if(ansNum==0)
+        {
+            document.getElementById("answer1").innerHTML = answerArray[questionType][(questionArray[questionType][questionNumber][false]) + 1];
+            document.getElementById("answer2").innerHTML = answerArray[Math.floor(Math.random() * numHomophoneTypes)][Math.floor(Math.random() * numHomophoneTypes) + 1];
+            document.getElementById("answer3").innerHTML = answerArray[Math.floor(Math.random() * numHomophoneTypes)][Math.floor(Math.random() * 2) + 1];
+        }
+        else if(ansNum==1)
+        {
+            document.getElementById("answer2").innerHTML = answerArray[questionType][(questionArray[questionType][questionNumber][false]) + 1];
+            document.getElementById("answer1").innerHTML = answerArray[Math.floor(Math.random() * numHomophoneTypes)][Math.floor(Math.random() * 2) + 1];
+            document.getElementById("answer3").innerHTML = answerArray[Math.floor(Math.random() * numHomophoneTypes)][Math.floor(Math.random() * 2) + 1];
+        }
+        else
+        {
+            document.getElementById("answer3").innerHTML = answerArray[questionType][(questionArray[questionType][questionNumber][false]) + 1];
+            document.getElementById("answer2").innerHTML = answerArray[Math.floor(Math.random() * numHomophoneTypes)][Math.floor(Math.random() * 2) + 1];
+            document.getElementById("answer1").innerHTML = answerArray[Math.floor(Math.random() * numHomophoneTypes)][Math.floor(Math.random() * 2) + 1];
+        }
+    }
+}
+
+function resetStatus()
+{
+    if(status == 1)
+        {
+            statusP.innerHTML = "Transport passenger!";
+        }
+        else if(status == 2)
+        {
+            statusP.innerHTML = "Drop off passenger!";
+        }
+        else
+        {
+            statusP.innerHTML = "Pick up passenger!";
+        }
+}
+
+function runGame()
+{
+    selectedDiv = 0;
+    status = 1;
+    influence = 0;
+    money = 0;
+    score = 0;
+    resetStatus();
+    bigCanvas.style.visibility = 'hidden';
+    play.style.visibility = 'visible';
+    play.innerHTML = "Play Game";
+//    gasCanvas.style.visibility = 'hidden';
+    numericStats.style.visibility = 'hidden';
+    difficultySelection.style.visibility = 'hidden';
+    answerSelection.style.visibility = 'hidden';
+    answerSelectionText.style.visibility = 'hidden';
+    answer1.style.visibility = 'hidden';
+    answer2.style.visibility = 'hidden';
+    answer3.style.visibility = 'hidden';
+//    removeElement("addGas");
+    initializeQuestions();
+}
+
+function removeElement(objId)
+{
+    var n = document.getElementById(objId);
+    n.parentNode.removeChild(n);
+}
+
+function selectDifficulty()
+{
+    play.style.visibility='hidden';
+    difficultySelection.style.visibility='visible';
+//    var addGasButton = document.createElement("button");
+//    addGasButton.innerHTML="Add Gas!";
+//    addGasButton.onclick = function(){addGas()};
+//    document.body.appendChild(addGasButton);
+}
+
+function playGame()
+{
+    var radios = dif1;
+    if(radios.checked)
+    {
+//        alert("Easy!");
+        difficulty=1;
+    }
+    var radios = dif2;
+    if(radios.checked)
+    {
+//        alert("Medium!");
+        difficulty=2;
+    }
+    var radios = dif3;
+    if(radios.checked)
+    {
+//        alert("Hard!");
+        difficulty=3;
+    }
+    
+//    gasCanvas.style.visibility='visible';
+    difficultySelection.style.visibility='hidden';
+    answerSelection.style.visibility = 'visible';
+    bigCanvas.style.visibility = 'visible';
+    numericStats.style.visibility = 'visible';
+    gasPercent=100;
+    time=0;
+    update();
+    startTimer();
+    askQuestion();
+}
+
+
+
+function addGas()
+{
+    if(addGasOn)
+    {
+        if(gasPercent<100&&money>0){
+        gasChanger=-0.4;
+        }
+        else{
+        gasChanger=0.03;
+        addGasOn=false;}
+    }
+    else
+    {
+        gasAdded=0;
+        addGasOn=true;
+    }
+}
+function startTimer() {
+    gameTick = window.setInterval(function(){requestAnimationFrame(tick)}, 10*((-1*difficulty)+4));
+}
+function changeGas()
+{
+    gasPercent-=gasChanger;
+    if(gasChanger<0&&money>0)
+    {
+        gasAdded+=(gasChanger*-1);
+        switch(difficulty)
+        {
+                case 1:
+                money-=0.1;
+                break;
+                case 2:
+                money-=0.2;
+                break;
+                case 3:
+                money-=0.3;
+                break;
+        }
+    }
+}
+function tick() {
+    changeGas();
+    if(addGasOn)
+    {
+        addGas(0);
+    }
+    gasPercent=gasPercent.toFixed(2);
+    update();
+}
+function update() {
+    time++;
+    multiplier+=0.00001;
+    score+=(difficulty*difficulty)*multiplier;
+    gameRunning=true;
+    if(gasPercent<0)
+    {
+        clearInterval(gameTick);
+        gasPercent=0;
+        gameRunning=false;
+    }
+    if(gameRunning)
+    {
+        numberDiv.innerHTML = ("Time: "+((time/(1000/(10*((-1*difficulty)+4)))).toFixed(0))+"<br>Score: "+score.toFixed(0));
+        moneyP.innerHTML = money.toFixed(2)+"$";
+        
+//        var drawingCanvas = document.getElementById('gasCanvas');
+//        var context = drawingCanvas.getContext("2d");
+        
+        document.getElementById('gasImageDiv').style.height = ((gasPercent/100)*245).toFixed(0);
+        
+//        context.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
+//        context.fillStyle="#8D9E3A";
+//        context.strokeStyle="#8D9E3A";
+//        context.beginPath();
+//        context.arc(drawingCanvas.height, drawingCanvas.height, drawingCanvas.height,(gasPercent/100)*Math.PI,(1+(gasPercent/100))*Math.PI);
+//        context.stroke();
+//        context.fill();
+//        context.fillStyle="#000000";
+//        context.font = (drawingCanvas.height/4+"pt Impact");
+//        context.fillText(""+gasPercent+"%",drawingCanvas.width/2-(drawingCanvas.height/3),(3*drawingCanvas.height)/3);
+    }
+    else
+    {
+        numberDiv.innerHTML = ((time/(1000/(10*((-1*difficulty)+4)))).toFixed(0));
+        clearInterval(gameTick);
+        if(time*difficulty*difficulty>highScore)
+        {
+            highScore=((time*difficulty*difficulty)/multiplier).toFixed(0);
+//            alert("New High Score: " + highScore);
+        }
+        else
+        {
+//            alert("High Score: "+highScore);
+        }
+        runGame();
+    }
+}
+
+function initializeQuestions()
+{
+    for(var i = 0; i < 2; i++)
+    {
+        answerArray[i] = new Array();
+        questionArray[i] = new Array();
+        for(var j = 0; j < 5; j++)
+        {
+            questionArray[i][j] = new Array();
+        }
+    }
+    //questionArray[homophone set][question number][true: question false: answer to question]
+    //answerArray[homophone set][number of questions in that set (first value only) OR value for each homophone (other members in array)]
+    var a = 0;
+    var on = 1;
+    answerArray[a][0] = 5;
+    answerArray[on][0] = 5;
+    
+    answerArray[a][1] = "a";
+    answerArray[a][2] = "\xE0";
+    answerArray[on][1] = "on";
+    answerArray[on][2] = "ont";
+    
+    questionArray[a][0][true] = "Hugues ___ d&eacutecid&eacute de suivre descours de guitare.";
+    questionArray[a][0][false] = 0;
+    
+    questionArray[a][1][true] = "Je n'ai plus envie d'aller ___ la mer.";
+    questionArray[a][1][false] = 1;
+    
+    questionArray[a][2][true] = "Louis ___ tellement chaud qu'il a mis son maillot.";
+    questionArray[a][2][false] = 0;
+    
+    questionArray[a][3][true] = "Philippe a gliss&eacute ___ cause de la flaque d'eau.";
+    questionArray[a][3][false] = 1;
+    
+    questionArray[a][4][true] = "Tu verras ma maison, elle ___ des volet verts.";
+    questionArray[a][4][false] = 0;
+    
+    questionArray[on][0][true] = "___ voulait aller &agrave l'op&eacutera, mais ils ont annul&eacute le r&eacutecital.";
+    questionArray[on][0][false] = 0;
+    
+    questionArray[on][1][true] = "Est-ce qu'ils ___ mis leur costume?";
+    questionArray[on][1][false] = 1;
+    
+    questionArray[on][2][true] = "Les professeurs ont entrepris une d&eacutemarche et ___ les appuie.";
+    questionArray[on][2][false] = 0;
+    
+    questionArray[on][3][true] = "Ils ___ trouv&eacute le voisin stationne devant la maison.";
+    questionArray[on][3][false] = 1;
+    
+    questionArray[on][4][true] = "___ a tout essay&eacute pour le r&eacutecup&eacuterer.";
+    questionArray[on][4][false] = 0;
+}
+
+
+
+
+
+
+
+
---
+++ gameplay.js
@@ -0,0 +1,233 @@
+var gameTick;
+var dodge = false;
+var taxiX = 544, dodgeCounter = 0;
+var y = 0, buildingy = -320, buildingy2 = 320, buildingy3 = 0;
+var y = 0, buildingy = -320, buildingy2 = 320, buildingy3 = 0;
+var obstructiony = 700;
+var buildingArray = new Array();
+var obstructionImage = new Image();
+
+function render(){
+    var canvas = document.getElementById("bigCanvas");
+    var g=canvas.getContext("2d");
+	g.clearRect(0, 0, canvas.width, canvas.height);
+	roadScroll();
+	taxi();
+	buildingSpawn();
+//	pedestrianWalk();
+	
+}
+
+function taxi(){
+	var canvas = document.getElementById("bigCanvas");
+    var g=canvas.getContext("2d");
+    var taxiImage = new Image();
+    taxiImage.src = "images/taxi.png";
+    g.drawImage(taxiImage, taxiX, 410);
+//	g.fillStyle = "#ffff00";
+//  g.fillRect(taxiX, 410, 64, 128);	
+}
+
+function sendObstruction(wasRight)
+{
+    if(wasRight)
+    {
+        dodge = true;
+    }
+    dodgeCounter=0;
+    obstructionImage.src = "images/Obstruction"+(Math.floor(Math.random()*1)+1)+".png";
+    obstructiony=-50;
+}
+
+function enviroment(){
+	var canvas = document.getElementById("bigCanvas");
+    var g=canvas.getContext("2d");
+	var roadImg;
+	var sidewalkImg;
+	var grassImg;
+	
+    taxi();
+//grass image
+	grassImg = new Image();
+	grassImg.src = "images/grasshizzle.png";
+	g.drawImage(grassImg, 0, y);
+	g.drawImage(grassImg, 0, y - 640);
+	
+//road image
+	roadImg = new Image();
+	roadImg.src = "images/road.png";
+	g.drawImage(roadImg, 384, y);
+	g.drawImage(roadImg, 384, y - 640);
+	
+//sidewalk image	
+	sidewalkImg = new Image();
+	sidewalkImg.src = "images/sidewalk.png";
+	g.drawImage(sidewalkImg, 288, y);
+	g.drawImage(sidewalkImg, 640, y);
+	g.drawImage(sidewalkImg, 288, y - 640);
+	g.drawImage(sidewalkImg, 640, y - 640);
+    
+    // Obstruction Image
+    g.drawImage(obstructionImage, 551, obstructiony);
+}
+
+function roadScroll(){
+	enviroment();
+	if(obstructiony<700)
+    {
+        obstructiony+=4;
+    }
+    if(dodge)
+    {
+        if(taxiX > 514)
+        {
+            taxiX-=1;
+        }
+        else if(taxiX > 480)
+        {
+            taxiX-=1.5;
+        }
+        else if(taxiX > 450)
+        {
+            taxiX-=1;
+        }
+        else
+        {
+            dodge = false;
+        }
+    }
+    else if(taxiX < 544)
+    {
+        if(dodgeCounter<40)
+        {
+            dodgeCounter++;
+        }
+        else
+        {
+            taxiX+=1;
+            if(taxiX > 480 && taxiX < 514)
+            {
+                taxiX+=0.5;
+            }
+        }
+    }
+    
+	y+=4;
+	if(y == 640){
+		y = 0;
+		enviroment();
+	}
+	buildingy+=4;
+    buildingy2+=4;
+    buildingy3+=4;
+    if(buildingy==640){
+        buildingy=-320;
+        buildingArray[0].src = getBuildingSrc();
+        buildingArray[1].src = getBuildingSrc();
+    }
+    if(buildingy2==640){
+        buildingy2=-320;
+        buildingArray[2].src = getBuildingSrc();
+        buildingArray[3].src = getBuildingSrc();
+    }
+    if(buildingy3==640){
+        buildingy3=-320;
+        buildingArray[4].src = getBuildingSrc();
+        buildingArray[5].src = getBuildingSrc();
+    }
+    
+}
+
+function pedestrian(){
+	
+	
+	
+}
+function pedestrianWalk(){
+	
+}
+
+function buildingSpawn(){
+	
+	var canvas = document.getElementById("bigCanvas");
+    var g=canvas.getContext("2d");
+	var x = 0;
+    for(var j = 0; j < 2; j++){
+        g.drawImage(buildingArray[j], x, buildingy);
+        x=800;
+    }
+    x = 0;
+    for(var j = 0; j < 2; j++){
+        g.drawImage(buildingArray[j+2], x, buildingy2);
+        x=800;
+    }
+    x = 0;
+    for(var j = 0; j < 2; j++){
+        g.drawImage(buildingArray[j+4], x, buildingy3);
+        x=800;
+    }
+	
+	
+	
+	
+}
+
+function getBuildingSrc(){
+//	if(Math.floor(Math.random()*9) != 0){
+		return "images/building"+(Math.floor(Math.random()*8)+1)+".png";
+//	}
+//	else{
+//		return "";
+////		alert("empty picture");
+//	}
+}
+
+function buildingScroll(){
+	
+}
+//function pedestrianWalk(){
+//	var onStreet 
+//	pedestrian(Math.floor(Math.random()*30), (Math.floor(Math.random()*64)+320));
+//	y+=5;
+//	
+//}
+//
+//function pedestrian(n, m){
+//	var canvas = document.getElementById("bigCanvas");
+//	var g=canvas.getContext("2d");
+//	
+//	blackhair = new Image();
+//	blackhair.src = "images/PersonTestHair.png";
+//	brownhair = new Image();
+//	brownhair.src = "images/PersonTestHair.png";
+//	blondehair = new Image();
+//	blondehair.src = "images/PersonTestHair.png";
+//	redhair = new Image();
+//	redhair.src = "images/PersonTestHair.png";
+//	grayhair = new Image();
+//	grayhair.src = "images/PersonTestHair.png";
+//
+//	if(n == 0){
+//		g.drawImage(blackhair, m, 0);
+//	}
+//	else if(n == 3){
+//		g.drawImage(brownhair, m, 0);
+//	}
+//	else if(n == 7){
+//		g.drawImage(blondehair, m, 0);
+//	}
+//	else if(n == 11){
+//		g.drawImage(redhair, m, 0);
+//	}
+//	else if(n == 15){
+//		g.drawImage(grayhair, m, 0);
+//	}
+//}
+
+function startTimerTy(){
+	for(var i = 0; i < 6; i++){
+		buildingArray[i] = new Image();
+		buildingArray[i].src = getBuildingSrc();
+	}
+		gameTick = window.setInterval(function(){render()}, 10);
+}
